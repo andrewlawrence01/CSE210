a4b3ecfec20840ef9804526be42f5a4509f42462
andrewlawrence01 <law22019@byui.edu>
2026-01-22 22:54:08 -0700

    commit to github

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Delivery Driver";
+        job1._company = "Domino's Pizza";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Front End Developer";
+        job2._company = "Epic";
+        job2._startYear = 2026;
+        job2._endYear = 2066;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Andrew Lawrence";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
